0ecec1fa946678a51fece78c9e6352a7eab72f31
FlailoftheLord <jeddyh15@gmail.com>
2019-11-19 21:32:52 -0500

    Make navbar shrink on page-scroll down

--- Resources/JavaScript/NavBarScroll.js
+++ Resources/JavaScript/NavBarScroll.js
@@ -1,23 +1,8 @@
 /*
  * Make the nav bar go away slightly when u scroll down.
- * 
  */
-
-var size = document.getElementByClass("content-box").scrollTop;
-
-window.onscroll = function() {
-
-	var nav-pane = document.getElementById("main-header");
-
-	var newSize = document.getElementByClass("content-box").scrollTop;
-
-	if (newSize < size) {
-		nav-pane.style.padding = "1.6%";
-
-	} else {    
-		nav-pane.style.padding = "2.3%";
-
-	}
-
-
-}
+ $(document).ready(function () {
+      $(window).on("scroll", function () {
+        $("body").toggleClass("scrolled", ($(window).scrollTop() > 80));
+      });
+ });
